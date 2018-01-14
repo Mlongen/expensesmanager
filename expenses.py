03f0aa6f58ee5bda9ee4af110ba9fa515f65ae0d
Mlongen <marcelolongen@gmail.com>
2018-01-13 21:27:04 -0800

    debug

--- expenses.py
+++ expenses.py
@@ -1,42 +1,44 @@
 import time
 
+try:
+    username = input("Enter an username:")
 
-username = input("Enter an username:")
+    fhand = open(username + ".txt", "r")
+    text = fhand.read()
+    now = time.strftime("%x")
+    while True:
+        n = input("Enter a command (for quick entry use -p.)(-help for usage instructions):")
+        if n == "new entry":
+            add = str(input("Please write the items name and value:)"))
+            if len(add) > 2:  # length needs to be at least 3.
+                output_file = open("output.txt", "a")
+                output_file.write("Name: " + add + " " + now +" \n")
+                print("Added.")
+                output_file.close()
+        elif n.startswith("-p"):
+                output_file = open("output.txt", "a")
+                output_file.write("Name: " + n[3:] + " " + now +" \n")
+                print("Added.")
+                output_file.close()
+        elif n.startswith("-help"):
+                fhand2 = open("help.txt", "r")
+                help = fhand2.read()
+                print(help)
+        elif n == "report":
+            report = open("output.txt", "r")
+            final = report.read()
+            capitalized = final.title()
+            words = final.split()
+            l = []
+            for word in words:
+                if word[0].isdigit() and word[2] != "/":
+                    l.append(int(word))
 
-fhand = open(username + ".txt", "r")
-text = fhand.read()
-now = time.strftime("%x")
-while True:
-    n = input("Enter a command (for quick entry use -p.)(-help for usage instructions):")
-    if n == "new entry":
-        add = str(input("Please write the items name and value:)"))
-        if len(add) > 2:  # length needs to be at least 3.
-            output_file = open("output.txt", "a")
-            output_file.write("Name: " + add + " " + now +" \n")
-            print("Added.")
-            output_file.close()
-    elif n.startswith("-p"):
-            output_file = open("output.txt", "a")
-            output_file.write("Name: " + n[3:] + " " + now +" \n")
-            print("Added.")
-            output_file.close()
-    elif n.startswith("-help"):
-            fhand2 = open("help.txt", "r")
-            help = fhand2.read()
-            print(help)
-    elif n == "report":
-        report = open("output.txt", "r")
-        final = report.read()
-        words = final.split()
-        l = []
-        for word in words:
-            if word[0].isdigit() and word[2] != "/":
-                l.append(int(word))
+            print(capitalized, "\n", "\n")
+            print("Total spent: CAD ", sum(l))
 
-        print(words)
-        print(final)
-        print("Total spent: CAD ", sum(l))
-
-
+except:
+    print("Invalid username. Terminating")
+    raise SystemExit
 
 
